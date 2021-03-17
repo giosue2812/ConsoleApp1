07dd9cacb6f7da908b5b6342a6917e4a5c0486f1
giosue2812 <giosue_liuzzo@hotmail.be>
2021-03-17 10:44:22 +0100

    Ajoutez des fichiers projet.

---
+++ Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Tools
+{
+    public class Command
+    {
+        internal string Query { get; private set; }
+        internal bool IsStoredProcedure { get; private set; }
+        internal Dictionary<string,object> Parameters { get; private set; }
+
+        public Command(string query, bool isStoredProcedure = false)
+        {
+            Query = query;
+            Parameters = new Dictionary<string, object>();
+            IsStoredProcedure = isStoredProcedure;
+        }
+
+        public void AddParameter(string parameterName, object value)
+        {
+            Parameters.Add(parameterName, value ?? DBNull.Value);
+        }
+    }
+}
---
+++ Connection.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Tools
+{
+    public class Connection
+    {
+        private readonly string _connectionString;
+        private readonly DbProviderFactory _factory;
+        public Connection(DbProviderFactory factory, string connectionString)
+        {
+            _factory = factory;
+            _connectionString = connectionString;
+            using (DbConnection connection = GetConnection())
+            {
+                connection.Open();
+            }
+        }
+        public int ExecuteNonQuery(Command command)
+        {
+            using (DbConnection connection = GetConnection())
+            {
+                using (DbCommand SqlCommand = CreateCommand(connection,command))
+                {
+                    connection.Open();
+                    return SqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+        public object ExecuteScalar(Command command)
+        {
+            using(DbConnection connection = GetConnection())
+            {
+                using(DbCommand SqlCommand = CreateCommand(connection,command))
+                {
+                    connection.Open();
+                    return SqlCommand.ExecuteScalar();
+                }
+            }
+        }
+        public IEnumerable<TResult> ExecuteReader<TResult>(Command command,Func<IDataRecord,TResult> selector)
+        {
+            using(DbConnection connection = GetConnection())
+            {
+                using(DbCommand sqlCommand = CreateCommand(connection, command))
+                {
+                    connection.Open();
+                    using (IDataReader dataReader = sqlCommand.ExecuteReader())
+                    {
+                        while(dataReader.Read())
+                        {
+                            yield return selector(dataReader);
+                        }
+                    }
+                }
+            }
+        }
+        public DataTable GetDataTable(Command command)
+        {
+            using (DbConnection connection = GetConnection())
+            {
+                using (DbCommand SqlCommand = CreateCommand(connection, command))
+                {
+                    connection.Open();
+                    using (DbDataAdapter dataAptater = _factory.CreateDataAdapter())
+                    {
+                        dataAptater.SelectCommand = SqlCommand;
+                        DataTable dataTable = new DataTable();
+                        dataAptater.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+
+        private DbConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = _connectionString;
+            return connection;
+        }
+
+        private DbCommand CreateCommand(DbConnection connection, Command command)
+        {
+            DbCommand sqlCommand = connection.CreateCommand();
+            sqlCommand.CommandText = command.Query;
+
+            if(command.IsStoredProcedure)
+            {
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+            }
+            foreach(KeyValuePair<string,object> kvp in command.Parameters)
+            {
+                DbParameter sqlParameter = _factory.CreateParameter();
+                sqlParameter.ParameterName = kvp.Key;
+                sqlParameter.Value = kvp.Value;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+            }
+            return sqlCommand;
+        }
+    }
+}
